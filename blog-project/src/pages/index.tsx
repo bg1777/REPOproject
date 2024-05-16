ddaa66552425b3391c479fe5d11b6e60f7d62c08
bg1777 <bgrodrigues2005@gmail.com>
2024-05-16 14:49:22 -0300

    :tada: Configuração inicial do projeto

---
+++ blog-project/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-[#F1E3F3] w-screen h-screen flex justify-center items-center" id="**FUNDO DA TELA**">
+      <div className="bg-[#fefefe] w-full h-full md:w-1/2 xl:w-1/3" id="**COLUNA DE POSTS**">
+        <div className="w-full border-b-2 h-16"></div>
+
+      </div>
+    </div>
+  );
+}
